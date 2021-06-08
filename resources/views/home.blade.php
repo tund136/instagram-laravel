14eed1024ff828bc0b5a93b535884cb2fea83743
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-06-08 17:01:27 +0700

    Adding Eloquent Relationships

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,11 +18,10 @@
                     <div class="pr-5"><strong>212</strong> following</div>
                 </div>
 
-                <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-                <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-                    massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+                <div>{{$user->profile->description}}
                 </div>
-                <div><a href="#">www.freecodecamp.org</a></div>
+                <div><a href="#">{{$user->profile->url}}</a></div>
             </div>
         </div>
 
