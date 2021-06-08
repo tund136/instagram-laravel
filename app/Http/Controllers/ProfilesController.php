b7571d26ad2eb5064989ec4dfbe1fc1e29baec65
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-06-08 17:24:49 +0700

    Fetching the Record From the Database

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -8,8 +8,7 @@ use Illuminate\Http\Request;
 class ProfilesController extends Controller {
 
     public function index($user_id) {
-        $users = User::all();
-        $user = $users->find($user_id);
+        $user = User::findOrFail($user_id);
 
         return view('home', ['user' => $user,]);
     }
