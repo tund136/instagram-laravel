7ed0857b8c8f225910170edbf6b85b92f3bed25a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-06-08 15:50:19 +0700

    Actions Handled By Resource Controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller {
-    
-    public function index() {
-        return view('home');
+
+    public function index($user_id) {
+        $users = User::all();
+        $user = $users->find($user_id);
+        dd($user);
+
+        //return view('home');
     }
 }
